93f413d4ded3aaa0b3b351cb41488d10f669a616
Avery.Davenport <averymdavenport@gmail.com>
2021-06-23 15:39:01 -0600

    second commit

--- movieList/movieList.test.ts
+++ movieList/movieList.test.ts
@@ -16,17 +16,17 @@ afterAll(async ()=> {
 });
 
 test('Add a movie to the page', async () => {
-    // let movieBar = await driver.findElement(By.name('movies'));
-
-    // await movieBar.sendKeys('Pans Labrynth\n');
-
-    // await movieBar.clear();
-
-    // await driver.findElement(By.xpath('//button[contains (text(), "x")]')).click();
-
-    // await driver.sleep(5000);
-
     let movieBar = await driver.findElement(By.name('movies'));
+
+    await movieBar.sendKeys('Pans Labrynth\n');
+
+    await movieBar.clear();
+
+    await driver.findElement(By.xpath('//button[contains (text(), "x")]')).click();
+
+    await driver.sleep(5000);
+
+    movieBar = await driver.findElement(By.name('movies'));
 
     await movieBar.sendKeys('Cabin in the Woods\n');
     await movieBar.sendKeys('test\n');
